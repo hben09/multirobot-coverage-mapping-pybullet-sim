6a813b56fcb13c2856223f12a908b2d2f9ab82c1
hben09 <hben09321@gmail.com>
2025-12-22 23:33:57 -0500

    Delete __init__.py

--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility modules for configuration loading."""
-
-# Geometry utilities have been moved to navigation.geometry
-# Import from navigation.geometry instead:
-#   from navigation.geometry import distance_to, angle_to, normalize_angle, heading_vector
